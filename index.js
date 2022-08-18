18e9cec3df6b8f70d345f478b1b7d8272b36a241
keith <nyakinyekeith@gmail.com>
2022-08-18 17:37:45 +0300

    stringsLabAssignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const currentUser='Grace Hopper';
+const welcomeMessage=`Welcome to Flatbook, ' ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const excitedshortGreeting= shortGreeting.charAt(0);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
 // Write your code in this file!
